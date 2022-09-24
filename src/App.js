3c983f0b084fb109f6002cbbf6bc6cc519be11d6
dam <yedam.kim@growthsoft.co.kr>
2022-09-24 21:26:31 +0900

    feat- 라우터 기본 구조 세팅

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-function App() {
-  return (
-    <div></div>
-  );
-}
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import MovieList from './routes/MovieList'
+import Movie from './routes/Movie'
 
-export default App;
+export default function App() {
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MovieList />} />
+      <Route path="/movies/:id" element={<Movie />} />
+    </Routes>
+  </BrowserRouter>
+}
--- src/routes/MovieList.js
+++ src/routes/MovieList.js
@@ -1,3 +1,3 @@
 export default function MovieList() {
-  return <div></div>
+  return <div>Movie List</div>
 }
