5fa8fe87ab72a16713ef7205173f3f969599f2c5
dam <yedam.kim@growthsoft.co.kr>
2022-09-24 21:40:12 +0900

    feat- graphql 기본 서버 세팅

---
+++ src/client.js
@@ -0,0 +1,19 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache()
+})
+
+client
+  .query({
+    query: gql`
+      {
+        allMovies {
+          title
+        }
+      }
+    `
+  })
+  .then((data) => console.log(data))
+export default cloent
